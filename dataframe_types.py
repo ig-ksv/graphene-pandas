5c783329d1d276d14799b535dbd65814ceaab95f
Igor Kozintsev <ig.kozintsev@gmail.com>
2021-01-04 22:33:27 +0200

    initial_setup

---
+++ dataframe_types.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import graphene
+
+from collections import OrderedDict
+
+from graphene.types.objecttype import ObjectType, ObjectTypeOptions
+from graphene.types.utils import yank_fields_from_attrs
+
+from resolvers import get_attr_resolver, get_custom_resolver
+
+
+def convert_field_type(column_type):
+    if pd.api.types.is_object_dtype(column_type):
+        field_type = graphene.List(graphene.String)
+    elif pd.api.types.is_integer_dtype(column_type):
+        field_type = graphene.List(graphene.Int)
+    elif pd.api.types.is_float_dtype(column_type):
+        field_type = graphene.List(graphene.Float)
+    elif pd.api.types.is_bool_dtype(column_type):
+        field_type = graphene.List(graphene.Boolean)
+    elif pd.api.types.is_datetime64_any_dtype(column_type):
+        field_type = graphene.List(graphene.DateTime)
+    else:
+        field_type = graphene.List(graphene.String)
+
+    return field_type
+
+
+def construct_fields(obj_type, model):
+    fields = OrderedDict()
+
+    field_id_type = convert_field_type(model.index.dtype)
+    fields["id"] = graphene.Field(resolver=lambda root, info: model.index.values,
+                                  type=field_id_type,
+                                  description="default id(DataFrame Index)",
+                                  required=None)
+
+    for column_name, column_type in model.dtypes.items():
+        field_description = f"Here must be description of a field"
+        field_resolver = get_custom_resolver(obj_type, column_name) or get_attr_resolver(model)
+        field_type = convert_field_type(column_type)
+
+        fields[column_name] = graphene.Field(resolver=field_resolver,
+                                             type=field_type,
+                                             description=field_description,
+                                             required=None)
+    return fields
+
+
+class DataFrameObjectTypeOptions(ObjectTypeOptions):
+    model = None
+    id = None
+
+
+class DataFrameObjectType(ObjectType):
+    @classmethod
+    def __init_subclass_with_meta__(
+            cls,
+            model=None,
+            skip_registry=False,
+            only_fields=(),
+            exclude_fields=(),
+            id=None,
+            _meta=None,
+            **options
+    ):
+        assert isinstance(model, pd.DataFrame), (
+            "You need to pass a valid pandas DataFrame in " '{}.Meta, received "{}".'
+        ).format(cls.__name__, model)
+
+        if only_fields and exclude_fields:
+            raise ValueError("The options 'only_fields' and 'exclude_fields' cannot be both set on the same type.")
+
+        pd_fields = yank_fields_from_attrs(
+            construct_fields(
+                obj_type=cls,
+                model=model,
+            ),
+            _as=graphene.Field,
+            sort=False,
+        )
+
+        if not _meta:
+            _meta = DataFrameObjectTypeOptions(cls)
+
+        _meta.model = model
+
+        if _meta.fields:
+            _meta.fields.update(pd_fields)
+        else:
+            _meta.fields = pd_fields
+
+        _meta.id = id or "id"
+
+        super(DataFrameObjectType, cls).__init_subclass_with_meta__(
+            _meta=_meta, **options
+        )
+
+    @classmethod
+    def get_data(cls, info):
+        model = cls._meta.model
+        return [model.to_dict()]
---
+++ tests/test_graphene_pandas.py
@@ -0,0 +1,110 @@
+import pytest
+import pandas as pd
+import numpy as np
+
+from graphene import Schema
+
+from dataframe_types import DataFrameObjectType
+
+
+class TestBasicQueries:
+    def _pre_setup(self, df: pd.DataFrame):
+        class DataFrameQuery(DataFrameObjectType):
+            class Meta:
+                model = df
+
+        self.schema = Schema(query=DataFrameQuery)
+
+    def test_string_integer_columns(self):
+        test_data = {"Brand": ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"],
+                     "Price": [22000, 25000, 27000, 35000]}
+
+        test_df = pd.DataFrame(test_data)
+        self._pre_setup(test_df)
+
+        query = """
+            query GrapheneDataFrame {
+              Price
+              Brand
+            }
+        """
+        result = self.schema.execute(query)
+        assert result.data["Brand"] == ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"]
+        assert result.data["Price"] == [22000, 25000, 27000, 35000]
+
+    def test_get_default_id_request(self):
+        test_data = {"Brand": ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"],
+                     "Price": [22000, 25000, 27000, 35000]}
+
+        test_df = pd.DataFrame(test_data)
+        self._pre_setup(test_df)
+
+        query = """
+            query GrapheneDataFrame {
+              id
+            }
+        """
+        result = self.schema.execute(query)
+        assert result.data["id"] == [0, 1, 2, 3]
+
+    def test_float_columns(self):
+        test_data = {"Price": [0.12, 234.583, 0.00002, 1936292676.7]}
+
+        test_df = pd.DataFrame(test_data)
+        self._pre_setup(test_df)
+
+        query = """
+            query GrapheneDataFrame {
+              Price
+            }
+        """
+        result = self.schema.execute(query)
+        assert result.data["Price"] == [0.12, 234.583, 0.00002, 1936292676.7]
+
+    def test_bool_columns(self):
+        test_data = {"BoolColumn": [True, False, False, True]}
+
+        test_df = pd.DataFrame(test_data)
+        self._pre_setup(test_df)
+
+        query = """
+            query GrapheneDataFrame {
+              BoolColumn
+            }
+        """
+        result = self.schema.execute(query)
+        assert result.data["BoolColumn"] == [True, False, False, True]
+
+    def test_nan_values(self):
+        test_data = {"Price": [0.12, None, 54.0, None]}
+
+        test_df = pd.DataFrame(test_data)
+        self._pre_setup(test_df)
+
+        query = """
+               query GrapheneDataFrame {
+                 Price
+               }
+           """
+        result = self.schema.execute(query)
+        assert len(list(filter(lambda x: np.isnan(x), result.data["Price"]))) == 2
+        assert len(result.data["Price"]) == 4
+
+    def test_date_datetime_values(self):
+        from datetime import datetime, date
+        test_data = {"Dates": [date(2012, 5, 11), date(2005, 2, 5)],
+                     "DateTimes": [datetime(2012, 5, 11, 10, 10), datetime(2005, 2, 5, 12, 30)]}
+
+        test_df = pd.DataFrame(test_data)
+        self._pre_setup(test_df)
+
+        query = """
+               query GrapheneDataFrame {
+                 Dates
+                 DateTimes
+               }
+           """
+        result = self.schema.execute(query)
+        assert result.data["Dates"] == ["2012-05-11", "2005-02-05"]
+        assert result.data["DateTimes"] == ["2012-05-11T10:10:00", "2005-02-05T12:30:00"]
+
