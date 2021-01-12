4c226192dced509c8b9d91104a1d24c8ed73382b
Igor Kozintsev <ig.kozintsev@gmail.com>
2021-01-12 23:20:29 +0200

    update tests

--- graphene_pandas/tests/test_graphene_pandas.py
+++ graphene_pandas/tests/test_graphene_pandas.py
@@ -1,19 +1,27 @@
-import pandas as pd
-import numpy as np
+import math
 
-from graphene import Schema
+import pandas as pd
+import graphene
 
 from graphene_pandas import DataFrameObjectType
 
 
 class TestBasicQueries:
-    def _pre_setup(self, df: pd.DataFrame, ex_fields=()):
-        class DataFrameQuery(DataFrameObjectType):
+    def _pre_setup(self, df: pd.DataFrame, ex_fields=(), on_fields=()):
+        class Records(DataFrameObjectType):
             class Meta:
                 model = df
                 exclude_fields = ex_fields
+                only_fields = on_fields
 
-        self.schema = Schema(query=DataFrameQuery)
+        class DataFrameQuery(graphene.ObjectType):
+            records = graphene.Field(Records, brand=graphene.String(), index=graphene.Int())
+
+            def resolve_records(self, info, index):
+                query = Records.get_row_int_index(info, index)
+                return query
+
+        self.schema = graphene.Schema(query=DataFrameQuery)
 
     def test_string_integer_columns(self):
         test_data = {"Brand": ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"],
@@ -22,30 +30,20 @@ class TestBasicQueries:
         test_df = pd.DataFrame(test_data)
         self._pre_setup(test_df)
 
-        query = """
-            query GrapheneDataFrame {
-              Price
-              Brand
-            }
+        test_index = 1
+
+        query = f"""
+            query Test {{
+              records(index: {test_index}) {{
+                Brand
+                Price
+              }}
+            }}
         """
         result = self.schema.execute(query)
-        assert result.data["Brand"] == ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"]
-        assert result.data["Price"] == [22000, 25000, 27000, 35000]
 
-    def test_get_default_id_request(self):
-        test_data = {"Brand": ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"],
-                     "Price": [22000, 25000, 27000, 35000]}
-
-        test_df = pd.DataFrame(test_data)
-        self._pre_setup(test_df)
-
-        query = """
-            query GrapheneDataFrame {
-              id
-            }
-        """
-        result = self.schema.execute(query)
-        assert result.data["id"] == [0, 1, 2, 3]
+        assert result.data["records"]["Brand"] == test_data["Brand"][test_index]
+        assert result.data["records"]["Price"] == test_data["Price"][test_index]
 
     def test_float_columns(self):
         test_data = {"Price": [0.12, 234.583, 0.00002, 1936292676.7]}
@@ -53,13 +51,18 @@ class TestBasicQueries:
         test_df = pd.DataFrame(test_data)
         self._pre_setup(test_df)
 
-        query = """
-            query GrapheneDataFrame {
-              Price
-            }
-        """
+        test_index = 1
+
+        query = f"""
+                   query Test {{
+                     records(index: {test_index}) {{
+                       Price
+                     }}
+                   }}
+               """
         result = self.schema.execute(query)
-        assert result.data["Price"] == [0.12, 234.583, 0.00002, 1936292676.7]
+
+        assert result.data["records"]["Price"] == test_data["Price"][test_index]
 
     def test_bool_columns(self):
         test_data = {"BoolColumn": [True, False, False, True]}
@@ -67,13 +70,18 @@ class TestBasicQueries:
         test_df = pd.DataFrame(test_data)
         self._pre_setup(test_df)
 
-        query = """
-            query GrapheneDataFrame {
-              BoolColumn
-            }
-        """
+        test_index = 1
+
+        query = f"""
+                   query Test {{
+                     records(index: {test_index}) {{
+                       BoolColumn
+                     }}
+                   }}
+               """
         result = self.schema.execute(query)
-        assert result.data["BoolColumn"] == [True, False, False, True]
+
+        assert result.data["records"]["BoolColumn"] == test_data["BoolColumn"][test_index]
 
     def test_nan_values(self):
         test_data = {"Price": [0.12, None, 54.0, None]}
@@ -81,14 +89,18 @@ class TestBasicQueries:
         test_df = pd.DataFrame(test_data)
         self._pre_setup(test_df)
 
-        query = """
-               query GrapheneDataFrame {
-                 Price
-               }
-           """
+        test_index = 1
+
+        query = f"""
+                   query Test {{
+                     records(index: {test_index}) {{
+                       Price
+                     }}
+                   }}
+               """
         result = self.schema.execute(query)
-        assert len(list(filter(lambda x: np.isnan(x), result.data["Price"]))) == 2
-        assert len(result.data["Price"]) == 4
+
+        assert result.data["records"]["Price"] == test_data["Price"][test_index]
 
     def test_date_datetime_values(self):
         from datetime import datetime, date
@@ -98,15 +110,20 @@ class TestBasicQueries:
         test_df = pd.DataFrame(test_data)
         self._pre_setup(test_df)
 
-        query = """
-               query GrapheneDataFrame {
-                 Dates
-                 DateTimes
-               }
-           """
+        test_index = 1
+
+        query = f"""
+                   query Test {{
+                     records(index: {test_index}) {{
+                       Dates
+                       DateTimes
+                     }}
+                   }}
+               """
         result = self.schema.execute(query)
-        assert result.data["Dates"] == ["2012-05-11", "2005-02-05"]
-        assert result.data["DateTimes"] == ["2012-05-11T10:10:00", "2005-02-05T12:30:00"]
+
+        assert result.data["records"]["Dates"] == "2005-02-05"
+        assert result.data["records"]["DateTimes"] == "2005-02-05T12:30:00"
 
     def test_exclude_fields(self):
         test_data = {"Brand": ["Honda Civic", "Toyota Corolla", "Ford Focus", "Audi A4"],
@@ -115,13 +132,18 @@ class TestBasicQueries:
         test_df = pd.DataFrame(test_data)
         self._pre_setup(test_df, ex_fields=("Price",))
 
-        query = """
-            query GrapheneDataFrame {
-              Brand
-              Price
-            }
-        """
+        test_index = 1
+
+        query = f"""
+                    query Test {{
+                      records(index: {test_index}) {{
+                        Brand
+                        Price
+                      }}
+                    }}
+                """
         result = self.schema.execute(query)
-        assert result.errors[0].message == 'Cannot query field "Price" on type "DataFrameQuery".'
+
+        assert result.errors[0].message == 'Cannot query field "Price" on type "Records".'
 
 
